3b273a704f7515f1d3260455311fcd479f62d586
aniaania321 <s31154@pjwstk.edu.pl>
2025-04-18 17:18:05 +0200

    class

--- src/Containers.API/Program.cs
+++ src/Containers.API/Program.cs
@@ -1,5 +1,13 @@
+using System.Net.Quic;
+using Containers.Application;
+using Containers.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("UniversityDatabase");
+
+builder.Services.AddTransient<IContainerService,ContainerService>(
+    _ =>new ContainerService(connectionString));
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -21,24 +29,36 @@ var summaries = new[]
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/api/containers", (IContainerService containerService) =>
+{
+    try
     {
-        var forecast = Enumerable.Range(1, 5).Select(index =>
-                new WeatherForecast
-                (
-                    DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                    Random.Shared.Next(-20, 55),
-                    summaries[Random.Shared.Next(summaries.Length)]
-                ))
-            .ToArray();
-        return forecast;
-    })
-    .WithName("GetWeatherForecast")
-    .WithOpenApi();
+        return Results.Ok(containerService.GetAllContainers());
+    }
+    catch (Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
+app.MapPost("/api/containers", (IContainerService containerService,Container container) =>
+{
+    try
+    {
+        var result = containerService.Create(container);
+        if (result is true)
+        {
+            return Results.Created();
+        }
+        else
+        {
+            return Results.BadRequest();
+        }
+    }
+    catch(Exception ex)
+    {
+        return Results.Problem(ex.Message);
+    }
+});
+
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
---
+++ src/Containers.Application/IContainerService.cs
@@ -0,0 +1,11 @@
+﻿
+
+using Containers.Models;
+
+namespace Containers.Application;
+
+public interface IContainerService
+{
+    IEnumerable<Container> GetAllContainers();
+    bool Create(Container container);
+}
